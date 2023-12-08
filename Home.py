e6c53418ddf580cabb6ac858a46b217b1e3e4f3a
Mohamed Selim Alouini <40542313+selim1897@users.noreply.github.com>
2023-12-08 05:10:56 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -15,25 +15,28 @@ for index, value in enumerate(countries['features']):
 
 type = st.radio(
     "",
-    ["Émissions annuelles moyennes de GES des trajets domicile-travail selon le département de résidence en 2019", 
-     "Distance domicile-travail moyenne selon le département de résidence en 2019", 
-     "Émissions annuelles moyennes de GES des voitures par adulte selon le département en 2019"])
+    ["Émissions annuelles moyennes de GES (Gaz à Effet de Serre) des trajets domicile-travail selon le département de résidence en 2019", 
+     "Distance domicile-travail moyenne (Km) selon le département de résidence en 2019", 
+     "Émissions annuelles moyennes de GES (Gaz à Effet de Serre) des voitures par adulte selon le département en 2019"])
 
 def importer_data(path):
       return 
 
-if type == "Émissions annuelles moyennes de GES des trajets domicile-travail selon le département de résidence en 2019":
+if type == "Émissions annuelles moyennes de GES (Gaz à Effet de Serre) des trajets domicile-travail selon le département de résidence en 2019":
     df = pd.read_excel("co2_deplacements.ods", engine="odf", sheet_name="Figure_1", header=3)
-elif type == "Distance domicile-travail moyenne selon le département de résidence en 2019":
+    color = "Viridis"
+elif type == "Distance domicile-travail moyenne (Km) selon le département de résidence en 2019":
     df = pd.read_excel("co2_deplacements.ods", engine="odf", sheet_name="Figure_1b_web", header=3)
+    color = "electric"
 else:
     df = pd.read_excel("co2_deplacements.ods", engine="odf", sheet_name="Fig_encadré_", header=3)
+    color = "portland"
 
 df = df[["Numéro de département", "Nom de département", df.columns[2]]]
 
 fig = go.Figure(go.Choroplethmapbox(geojson=countries,locations=df['Numéro de département'], z=df[df.columns[2]],
-                                    colorscale="Viridis", zmin=min(df[df.columns[2]]), zmax=max(df[df.columns[2]]),
-                                    marker_opacity=0.5, marker_line_width=0 ))
+                                    colorscale=color , zmin=min(df[df.columns[2]]), zmax=max(df[df.columns[2]]),
+                                    marker_opacity=0.6, marker_line_width=0.5 ))
 fig.update_layout(mapbox_style="carto-positron",
                   mapbox_zoom=4.5, mapbox_center = {"lat": 46.8566, "lon": 2.3522})
 
